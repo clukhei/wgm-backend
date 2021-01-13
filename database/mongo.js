47166ec8680685542f1696a94e0c5bf3a922915e
Lu Khei <kheiboardwarrior@gmail.com>
2021-01-13 22:25:51 +0800

    use mongo atlas'

--- database/mongo.js
+++ database/mongo.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const {MongoClient, ObjectId} = require('mongodb')
 
-const mongoClient = new MongoClient(process.env.MONGO_LOCAL_URL, {
+const mongoClient = new MongoClient(process.env.MONGO_URL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 })
